16c52e257f1c223bc32a24a4888c102c5d9d5fb9
Daren McCune <darenmccune@gmail.com>
2019-08-28 18:28:46 -0700

    Updated to python 3

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ from algorithm import Game
 x=Game(8, 8)
 for cell in [0,33, 34, 41, 42]:
     x.flip_physical_board_cell(cell)
-print x.board.astype(int)
+print(x.board.astype(int))
 
 x.playing=True
 for i in range(10):
-    print "continue ##########################"
+    print("continue ##########################")
     #raw_input("continue? ")
     x.tick()
-    print "virtual board:"
-    print x.board.astype(int)
-    print "Physical:"
-    print x._get_physical_board().astype(int)
+    print("virtual board:")
+    print(x.board.astype(int))
+    print("Physical:")
+    print(x._get_physical_board().astype(int))
